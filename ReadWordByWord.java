0c6ba738c0c8c565553fc65168c397fe659371a7
442012310 <91116602+442012310@users.noreply.github.com>
2021-12-16 07:52:24 +0300

    Create ReadWordByWord.java

---
+++ ReadWordByWord.java
@@ -0,0 +1,6 @@
+public class ReadWordByWord{
+        public static void main(String[] args) {
+                String name = "Khaled\nAbdulaziz";
+                System.out.println(name);
+        }
+}
